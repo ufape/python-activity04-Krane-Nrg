23a8bcd06d8d1dd3edf93f71cb59f8248b953584
Krane-Nrg <142512782+Krane-Nrg@users.noreply.github.com>
2023-09-01 18:00:42 -0300

    Update library_problem_1.py

--- library_problem_1.py
+++ library_problem_1.py
@@ -1,9 +1,9 @@
 def media ():
 
-    n1 = float(input("Digite a nota 1: "))
-    n2 = float(input("Digite a nota 2: "))
-    n3 = float(input("Digite a nota 3: "))
-    n4 = float(input("Digite a nota 4: "))
+    N1 = float(input("Digite a nota 1: "))
+    N2 = float(input("Digite a nota 2: "))
+    N3 = float(input("Digite a nota 3: "))
+    N4 = float(input("Digite a nota 4: "))
     media = (n1 * 2 + n2 * 3 + n3 * 4 + n4 * 1) / 10
 
     if media >= 7.0:
